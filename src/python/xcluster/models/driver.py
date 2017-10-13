098ec976b7cf9445a2b878d8005951e677ec827c
David T <dterovanesyan@gmail.com>
2017-10-12 22:33:51 -0400

    new commit

--- src/python/xcluster/models/driver.py
+++ src/python/xcluster/models/driver.py
@@ -7,8 +7,10 @@ def loadData(filename):
 
 
 def main():
-    print "hi"
+
     data = loadData("../../../../data/glass.tsv")
+    print data.shape
+
     pca = PCA(data)
     print pca.shape
     print pca
