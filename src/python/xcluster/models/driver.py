c1a4953ef13e9f0f22b46b0d25f2a4e8091219c0
David T <dterovanesyan@gmail.com>
2017-10-20 10:49:03 -0400

    new driver

--- src/python/xcluster/models/driver.py
+++ src/python/xcluster/models/driver.py
@@ -31,10 +31,10 @@ def main():
     #speaker has 600 dimensions
     #ilsvrc12_50k has 2048 dimensions
 
-    pca = PCA(np.array(p), 1536)
+    pca = PCA(np.array(p), 1024)
     print pca.shape
 
-    f= open("../../../../data/PCA_data_ilsvrc12_dim-1536.tsv", "w")
+    f= open("../../../../data/PCA_data_ilsvrc12_dim-1024.tsv", "w")
     #f= open("../../../../data/PCA_data.tsv", "w")
     #f= open("../../../../data/PCA_data_dim-7.tsv", "w")
     #f= open("../../../../data/PCA_data_speaker.tsv", "w")
