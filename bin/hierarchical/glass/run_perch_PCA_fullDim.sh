dda7138e192b7c6c09376a2e58d0c98d50ca4d0c
David T <dterovanesyan@gmail.com>
2017-11-09 23:41:04 -0500

    new driver

--- bin/hierarchical/glass/run_perch_PCA_fullDim.sh
+++ bin/hierarchical/glass/run_perch_PCA_fullDim.sh
@@ -4,12 +4,12 @@ set -exu
 
 output_dir=${1:-"experiments_out"}
 
-#dataset_file=$XCLUSTER_ROOT/data/glass.tsv
 dataset_file=$XCLUSTER_ROOT/data/PCA_data_glass_fullDim.tsv
-#dataset_name=glass
 dataset_name=PCA_data_glass_fullDim
+
 num_runs=10
-num_threads=24
+num_threads=4
+par_max_frontier=50
 
 mkdir -p $output_dir
 
@@ -20,12 +20,13 @@ expected_dp_point_file="None"
 
 for i in `seq 1  $num_runs`
     do
-        algorithm_name="Perch-py"
+        algorithm_name="Perch with dim 3"
         shuffled_data="${dataset_file}.$i"
         exp_output_dir="$output_dir/$dataset_name/$algorithm_name/run_$i"
 
-        python -m xcluster.eval.eval_dataset --input $shuffled_data --outdir $exp_output_dir \
-        --algorithm $algorithm_name --dataset $dataset_name --max_leaves None --clusters None
+        java -Xmx20G -cp $XCLUSTER_JARPATH xcluster.eval.RunPerch --input $shuffled_data --outdir $exp_output_dir \
+        --algorithm $algorithm_name --dataset $dataset_name --max-leaves None --clusters None --threads $num_threads \
+        --max-frontier-par $par_max_frontier
 
         sh bin/util/score_tree.sh \
         $exp_output_dir/tree.tsv $algorithm_name $dataset_name $num_threads $expected_dp_point_file \
