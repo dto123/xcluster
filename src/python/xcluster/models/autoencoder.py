55e1bd38888251fb7d61f33304a7227a719b0943
David T <dterovanesyan@gmail.com>
2018-03-06 18:40:00 -0500

    new auto

--- src/python/xcluster/models/autoencoder.py
+++ src/python/xcluster/models/autoencoder.py
@@ -120,8 +120,8 @@ class AutoEncoder(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(AutoEncoder, self).__init__()
         #d_prime_1 is longer one
-        d_prime = (input_dim+output_dim)/2
-        #d_prime = input_dim
+        #d_prime = (input_dim+output_dim)/2
+        d_prime = input_dim
         self.encoder = nn.Sequential(
 
             #1 One Hidden Layer
