f33cdce3bb9371b3497837eb82712c8f707fd98b
David T <dterovanesyan@gmail.com>
2018-03-06 17:38:38 -0500

    new autoencoder

--- src/python/xcluster/models/driver.py
+++ src/python/xcluster/models/driver.py
@@ -40,7 +40,7 @@ def main():
     train_dim, new_dim =encoder.shape
     #print pca.shape
     #autoencoder = autoencoder(np.array(p), 2)
-    f= open("../../../../data/Autoencoder_data_glass_dim_2.tsv", "w")
+    f= open("../../../../data/Autoencoder_data_glass_dim_2_oneHL.tsv", "w")
     #f= open("../../../../data/PCA_data_glass_fullDim.tsv", "w")
     #f= open("../../../../data/PCA_data_dim-3.tsv", "w")
     #f= open("../../../../data/PCA_data_dim-7.tsv", "w")
