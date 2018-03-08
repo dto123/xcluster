f9b062c3eb8ba0760187a863dd6d481f3523f787
David T <dterovanesyan@gmail.com>
2018-03-08 14:06:27 -0500

    dim 3

--- src/python/xcluster/models/autoencoder.py
+++ src/python/xcluster/models/autoencoder.py
@@ -110,8 +110,8 @@ def reduceData(data, output_dim):
     encoded_data, _ = autoencoder(view_data)
 
     print(encoded_data.shape)
-    return encoded_data.data[0]
-    #return encoded_data
+    #return encoded_data.data[0]
+    return encoded_data
 
 
 
