bb393f16d9c626f97669bf7b9655c2493d80a58b
David T <dterovanesyan@gmail.com>
2018-04-04 17:31:54 -0400

    AA BB

--- src/python/xcluster/utils/AA bouding boxes.py
+++ src/python/xcluster/utils/AA bouding boxes.py
@@ -3,7 +3,7 @@ from deltasep_utils import gen_k_centers
 
 def main():
     centers, delta = gen_k_centers(3, 2)
-    print "hi"
+    print ("hi")
     print centers
     return centers
     return delta
