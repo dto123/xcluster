7395141065828eeb641820e847f3066af2cb5c4c
David T <dterovanesyan@gmail.com>
2018-04-04 18:53:51 -0400

    AA BB

--- src/python/xcluster/utils/AA bouding boxes.py
+++ src/python/xcluster/utils/AA bouding boxes.py
@@ -1,17 +1,22 @@
 from deltasep_utils import gen_k_centers
-import random
+import numpy as np
 
 def main():
     centers, delta = gen_k_centers(2, 2)
     corners = []
+    datapoints = []
     for center in centers:
-        x = random.uniform(0, 1)
-        y = random.uniform(0, 1)
+        x = np.random.uniform(0, 1)
+        y = np.random.uniform(0, 1)
         corner1_x = (center[0] - (0.5*x*delta))
         corner1_y = (center[1] - (0.5*y*delta))
         corner2_x = (center[0] + (0.5*x*delta))
         corner2_y = (center[1] + (0.5*y*delta))
-        corners.append((corner1_x, corner1_y))
-        corners.append((corner2_x, corner2_y))
-    print (corners)
+
+        datapoints_x = np.random.uniform(low=corner1_x, high = corner2_x, size = (25,))
+        datapoints_y = np.random.uniform(low=corner1_y, high = corner2_y, size = (25,))
+
+        datapoints = np.vstack((datapoints_x, datapoints_y)).T
+        print datapoints.shape()
+
 main()
