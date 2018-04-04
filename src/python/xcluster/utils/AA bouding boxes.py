592511861861c5b736774ac86ec32a1a10b479cc
David T <dterovanesyan@gmail.com>
2018-04-04 17:49:03 -0400

    AA BB

--- src/python/xcluster/utils/AA bouding boxes.py
+++ src/python/xcluster/utils/AA bouding boxes.py
@@ -11,6 +11,7 @@ def main():
         corner1_y = (center[1] - (0.5*y*delta))
         corner2_x = (center[0] + (0.5*x*delta))
         corner2_y = (center[1] + (0.5*y*delta))
-        corners.append((corner1_x, corner1_y), corner2_x, corner2_y)
+        corners.append(corner1_x, corner1_y)
+        corners.append(corner2_x, corner2_y)
     print (corners)
 main()
