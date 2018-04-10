7908ca7730d7aa82371c2eb92ea0cceb995ed490
David T <dterovanesyan@gmail.com>
2018-04-10 17:53:15 -0400

    new BB

--- src/python/xcluster/utils/AA bouding boxes.py
+++ src/python/xcluster/utils/AA bouding boxes.py
@@ -3,9 +3,10 @@ import numpy as np
 
 def main():
     dim = 2
-    centers, delta = gen_k_centers(4, dim)
+    centers, delta = gen_k_centers(3, dim)
     list_of_data = []
     rotated_data = []
+    clusterList = []
     for center in centers:
         x = np.random.uniform(0, 1)
         y = np.random.uniform(0, 1)
@@ -18,22 +19,41 @@ def main():
         datapoints_y = np.random.uniform(low=corner1_y, high = corner2_y, size = (25,))
 
         random_datapoints = np.transpose(np.vstack((datapoints_x, datapoints_y)))
-        print (random_datapoints.shape)
+        #print (random_datapoints.shape)
         list_of_data.append(random_datapoints)
+        cluster = 1
+        for i in range(25):
+            clusterList.append(np.array(cluster))
+        cluster++
 
 
 
-    print (len(list_of_data))
+
+
+    #print (len(list_of_data))
     W = np.random.rand(dim,dim)
     for points in list_of_data:
         newpoints = np.matmul(points, W)
         rotated_data.append(newpoints)
-    print (len(rotated_data))
+    #print (len(rotated_data))
 
     final_data = np.vstack(list_of_data)
     final_rotated_data = np.vstack(rotated_data)
 
+    d1, d2 = final_data.shape
+    pidList = []
+    for a in range(d1):
+        one = np.array(a)
+        newList.append(one)
+
+    clusterList = np.asarray(clusterList)[...,None]
+    pidList = np.asarray(pidList)[...,None]
+
+
     print (final_data.shape)
     print (final_rotated_data.shape)
+
+    print (pidList.shape)
+    print (clusterList.shape)
 
 main()
