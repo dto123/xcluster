707f34d1d22ecfe3190a35210044fda4d34c04c3
David T <dterovanesyan@gmail.com>
2018-04-12 17:37:26 -0400

    high dim

--- src/python/xcluster/utils/AA bouding boxes.py
+++ src/python/xcluster/utils/AA bouding boxes.py
@@ -76,13 +76,13 @@ def main():
     #Finalized_data = np.hstack((pidList, last_data))
 
 
-    f= open("../../../../data/100d_nonprojected_data_75_clusters.tsv", "w")
+    f= open("../../../../data/100d_projected_data_75_clusters.tsv", "w")
 
     for i in range(d1):
         line = []
         line.append(pidList[i])
         line.append(clusterList[i])
-        line.extend(list(final_data[i,:]))
+        line.extend(list(final_rotated_data[i,:]))
         f.write("%s\n"%"\t".join([str(x) for x in line]))
         #print("%s\n"%"\t".join([str(x) for x in line]))
 
