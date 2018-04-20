195986e60f3c98468d051718249ce08d55ab12f1
David T <dterovanesyan@gmail.com>
2018-04-20 17:43:28 -0400

    linearAE

--- src/python/xcluster/models/autoencoder.py
+++ src/python/xcluster/models/autoencoder.py
@@ -138,7 +138,7 @@ def reduceData(data, output_dim):
     plt.ylabel('Loss')
     plt.xlabel('Iterations')
     plt.title('Iterations vs Loss')
-    plt.savefig('LossVsIterationsIlsvrc.png')
+    plt.savefig('LossVsIterationsLinear.png')
 
 
     # visualize in 3D plot
@@ -179,10 +179,11 @@ class AutoEncoder(nn.Module):
             #nn.Tanh(),
             #3 Two Hidden Layers
 
-            nn.Linear(input_dim, d_prime),
-            nn.Tanh(),
+            nn.Linear(input_dim, output_dim),
+            #nn.Linear(input_dim, d_prime),
+            #nn.Tanh(),
             #nn.ReLU(),
-            nn.Linear(d_prime, output_dim),
+            #nn.Linear(d_prime, output_dim),
             #nn.Tanh(),
 
         )
@@ -198,10 +199,11 @@ class AutoEncoder(nn.Module):
             #d_prime_1 = (input_dim+output_dim)/2
             #d_prime_2 = input_dim
 
-            nn.Linear(output_dim, d_prime),
-            nn.Tanh(),
+            nn.Linear(output_dim, input_dim),
+            #nn.Linear(output_dim, d_prime),
+            #nn.Tanh(),
             #nn.ReLU(),
-            nn.Linear(d_prime, input_dim),
+            #nn.Linear(d_prime, input_dim),
             #nn.Tanh(),
             #3 Two Hidden Layers
 
