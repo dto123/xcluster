83185fe4839c1bf2885f853f8f5ef199938d9404
David T <dterovanesyan@gmail.com>
2018-04-21 23:17:25 -0400

    newglass

--- src/python/xcluster/models/driver.py
+++ src/python/xcluster/models/driver.py
@@ -159,7 +159,7 @@ def main():
 
 
     #pca = PCA(np.array(p), 2)
-    encoder = reduceData(np.array(p), 5)
+    encoder = reduceData(np.array(p), 1024)
     #encoder = reduceData(np.array(projectedP), 5)
 
     print (encoder.shape)
